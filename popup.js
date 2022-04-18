44efa512515442471cb0b6a50307595d90070166
Sagar Chauhan <coolsagar005@gmail.com>
2022-04-19 00:12:43 +0530

    0.0.4

--- popup.js
+++ popup.js
@@ -17,6 +17,12 @@ bypass.addEventListener("click", async () => {
 // The body of this function will be execuetd as a content script inside the
 // current page
 function generateBypassLink(url) {
+  // check if the url is a medium article
   let new_url = "https://12ft.io/"+url;
+  if(url.includes("medium.com")){
+    new_url = "https://postscripts.medium.com/bypass-paywall-so-this-happened-2e3de621335";
+  }
+  console.log("Medium article cannot be bypassed, for more information please visit: https://postscripts.medium.com/bypass-paywall-so-this-happened-2e3de621335");
+  console.log("new_url", new_url);
   window.open(new_url, "_blank");
 }
